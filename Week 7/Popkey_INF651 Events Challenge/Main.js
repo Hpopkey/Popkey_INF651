d6fb4c7d2226c675e28df1f97aa257a8e983c099
Hpopkey <hdpopkey@mail.fhsu.edu>
2025-07-14 21:21:55 -0500

    Added Week 7 assignment, Events Challenge

---
+++ Week 7/Popkey_INF651 Events Challenge/Main.js
@@ -0,0 +1,29 @@
+let output = document.getElementById("coordinates")
+let box = document.getElementById("mouseTracker");
+let submit = document.getElementById('submitButton').addEventListener('click', handleSubmit);
+let divOutput = document.getElementById('outputDiv');
+
+function handleSubmit() {
+    let itemInput = document.getElementById('nameInput').value.trim();    //Form submission function
+
+    if (itemInput === '') {
+        divOutput.textContent = "Error: Please enter a name.";
+        divOutput.style.backgroundColor = 'red';
+    } else {
+        divOutput.textContent = "Welcome, " + itemInput + "!";
+        divOutput.style.backgroundColor = "green";
+    }
+}
+
+document.getElementById('nameInput').addEventListener('keypress', function(event) {  // Keyboard event
+    if (event.key === 'Enter' ) {
+        handleSubmit();
+    }
+});
+
+
+box.addEventListener("mousemove", runEvent);
+
+function runEvent(e){  // Mouse event
+    output.innerHTML = "<h3>Mouse X:" + e.offsetX + "</h3><h3>Mouse Y: " + e.offsetY + "</h3>";
+}
